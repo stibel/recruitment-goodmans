291b900c068a533ca708f3f55001cb8f26d93016
stibel <mikolaj.siebielec@skysnap.pl>
2022-06-15 22:00:56 +0200

    Created simple image preview display

--- src/components/page/index.tsx
+++ src/components/page/index.tsx
@@ -7,6 +7,7 @@ const StyledDiv = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
+  flex-flow: column;
   justify-content: center;
   align-items: center;
 `;
--- src/pages/photo-page.tsx
+++ src/pages/photo-page.tsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { FileList } from "../components/file-list";
 import { FileUpload } from "../components/file-upload";
 import { Page } from "../components/page";
 import { useArray } from "../shared/hooks/useArray";
 import { createUrl } from "../shared/utils";
+
+const ImageWrapper = styled.div`
+  /* position: relative; */
+  width: 100%;
+  height: 75%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f0ffff;
+`;
+
+const StyledImg = styled.img`
+  object-fit: contain;
+  height: 100%;
+`;
 
 const FileControlWrapper = styled.div`
   width: 100%;
@@ -23,12 +38,23 @@ export const PhotoPage = () => {
   } = useArray<File>([]);
   const [activePhoto, setActivePhoto] = useState<string>("");
 
+  useEffect(() => {
+    if (!photos.length) {
+      setActivePhoto("");
+      return;
+    }
+    setActivePhoto(createUrl(photos[0]));
+  }, [photos]);
+
   const onChange = (files: Array<File>) => {
     setPhotos(files);
   };
 
   return (
     <Page>
+      <ImageWrapper>
+        <StyledImg src={activePhoto} alt={"active photo"} />
+      </ImageWrapper>
       <FileControlWrapper>
         <FileList files={photos} />
         <FileUpload onChange={onChange} />
